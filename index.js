532f1c3ae4e335d405590f035625ca489f3a454e
Ajied21 <dhoifullah.luthmajied05@gmail.com>
2025-08-31 16:11:25 +0700

    chores(ci): fixs add per-job lint, test and sonarqube

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-import app from './src/app.js';
+// index.js
+import app from './src/app.js';  // pastikan src/app.js juga pakai export default
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`notes-app listening on :${PORT}`);
-});
+app.listen(PORT, () => console.log(`notes-app listening on :${PORT}`));
 
